5122a29e262315e38cd8872b6d949ee87db764fa
Julien QUENTIN <jqntn88@gmail.com>
2024-01-23 21:07:03 +0100

    clean default sample

--- projects/JuGame/Classes/AppDelegate.cpp
+++ projects/JuGame/Classes/AppDelegate.cpp
@@ -22,10 +22,10 @@
  THE SOFTWARE.
  ****************************************************************************/
 
+#define USE_AUDIO_ENGINE 0
+
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
-
-// #define USE_AUDIO_ENGINE 1
 
 #if USE_AUDIO_ENGINE
 #include "audio/include/AudioEngine.h"
@@ -34,10 +34,18 @@ using namespace cocos2d::experimental;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+static constexpr auto GAME_NAME = "JuGame";
+
+static const auto designResolutionSize = cocos2d::Size(480, 320);
+static const auto smallResolutionSize = cocos2d::Size(480, 320);
+static const auto mediumResolutionSize = cocos2d::Size(1024, 768);
+static const auto largeResolutionSize = cocos2d::Size(2048, 1536);
+
+static int
+register_all_packages()
+{
+  return 0;
+}
 
 AppDelegate::AppDelegate() {}
 
@@ -48,71 +56,54 @@ AppDelegate::~AppDelegate()
 #endif
 }
 
-// if you want a different context, modify the value of glContextAttrs
-// it will affect all platforms
 void
 AppDelegate::initGLContextAttrs()
 {
-  // set OpenGL context attributes:
   // red,green,blue,alpha,depth,stencil,multisamplesCount
-  GLContextAttrs glContextAttrs = { 8, 8, 8, 8, 24, 8, 0 };
+  GLContextAttrs glContextAttrs = { 8, 8, 8, 8, 24, 8, 4 };
 
   GLView::setGLContextAttrs(glContextAttrs);
-}
-
-// if you want to use the package manager to install more packages,
-// don't modify or remove this function
-static int
-register_all_packages()
-{
-  return 0; // flag for packages manager
 }
 
 bool
 AppDelegate::applicationDidFinishLaunching()
 {
-  // initialize director
-  auto director = Director::getInstance();
+  const auto director = Director::getInstance();
+
   auto glview = director->getOpenGLView();
-  if (!glview) {
+
+  if (nullptr == glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) ||                               \
   (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) ||                                   \
   (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-    glview = GLViewImpl::createWithRect(
-      "JuGame",
-      cocos2d::Rect(
-        0, 0, designResolutionSize.width, designResolutionSize.height));
+    const auto& size = designResolutionSize;
+    const auto rect = Rect(0, 0, size.width, size.height);
+
+    glview = GLViewImpl::createWithRect(GAME_NAME, rect);
 #else
-    glview = GLViewImpl::create("JuGame");
+    glview = GLViewImpl::create(GAME_NAME);
 #endif
+
     director->setOpenGLView(glview);
   }
 
-  // turn on display FPS
   director->setDisplayStats(true);
-
-  // set FPS. the default value is 1.0/60 if you don't call this
   director->setAnimationInterval(1.0f / 60);
 
-  // Set the design resolution
   glview->setDesignResolutionSize(designResolutionSize.width,
                                   designResolutionSize.height,
                                   ResolutionPolicy::NO_BORDER);
-  auto frameSize = glview->getFrameSize();
-  // if the frame's height is larger than the height of medium size.
+
+  const auto frameSize = glview->getFrameSize();
   if (frameSize.height > mediumResolutionSize.height) {
     director->setContentScaleFactor(
       MIN(largeResolutionSize.height / designResolutionSize.height,
           largeResolutionSize.width / designResolutionSize.width));
-  }
-  // if the frame's height is larger than the height of small size.
-  else if (frameSize.height > smallResolutionSize.height) {
+  } else if (frameSize.height > smallResolutionSize.height) {
     director->setContentScaleFactor(
       MIN(mediumResolutionSize.height / designResolutionSize.height,
           mediumResolutionSize.width / designResolutionSize.width));
-  }
-  // if the frame's height is smaller than the height of medium size.
-  else {
+  } else {
     director->setContentScaleFactor(
       MIN(smallResolutionSize.height / designResolutionSize.height,
           smallResolutionSize.width / designResolutionSize.width));
@@ -120,17 +111,13 @@ AppDelegate::applicationDidFinishLaunching()
 
   register_all_packages();
 
-  // create a scene. it's an autorelease object
-  auto scene = HelloWorld::createScene();
+  const auto scene = HelloWorld::createScene();
 
-  // run
   director->runWithScene(scene);
 
   return true;
 }
 
-// This function will be called when the app is inactive. Note, when receiving a
-// phone call it is invoked.
 void
 AppDelegate::applicationDidEnterBackground()
 {
@@ -141,7 +128,6 @@ AppDelegate::applicationDidEnterBackground()
 #endif
 }
 
-// this function will be called when the app is active again
 void
 AppDelegate::applicationWillEnterForeground()
 {
--- projects/JuGame/Classes/HelloWorldScene.cpp
+++ projects/JuGame/Classes/HelloWorldScene.cpp
@@ -26,13 +26,6 @@
 
 USING_NS_CC;
 
-Scene*
-HelloWorld::createScene()
-{
-  return HelloWorld::create();
-}
-
-// Print useful error message instead of segfaulting when files are not there.
 static void
 problemLoading(const char* filename)
 {
@@ -41,73 +34,57 @@ problemLoading(const char* filename)
          "front of filenames in HelloWorldScene.cpp\n");
 }
 
-// on "init" you need to initialize your instance
+Scene*
+HelloWorld::createScene()
+{
+  return HelloWorld::create();
+}
+
 bool
 HelloWorld::init()
 {
-  //////////////////////////////
-  // 1. super init first
   if (!Scene::init()) {
     return false;
   }
 
-  auto visibleSize = Director::getInstance()->getVisibleSize();
-  Vec2 origin = Director::getInstance()->getVisibleOrigin();
+  const auto visibleSize = Director::getInstance()->getVisibleSize();
+  const auto visibleOrigin = Director::getInstance()->getVisibleOrigin();
 
-  /////////////////////////////
-  // 2. add a menu item with "X" image, which is clicked to quit the program
-  //    you may modify it.
-
-  // add a "close" icon to exit the progress. it's an autorelease object
-  auto closeItem =
+  const auto closeItem =
     MenuItemImage::create("CloseNormal.png",
                           "CloseSelected.png",
                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
 
-  if (closeItem == nullptr || closeItem->getContentSize().width <= 0 ||
+  if (nullptr == closeItem || closeItem->getContentSize().width <= 0 ||
       closeItem->getContentSize().height <= 0) {
     problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
   } else {
-    float x =
-      origin.x + visibleSize.width - closeItem->getContentSize().width / 2;
-    float y = origin.y + closeItem->getContentSize().height / 2;
+    const auto x = visibleOrigin.x + visibleSize.width -
+                   closeItem->getContentSize().width / 2;
+    const auto y = visibleOrigin.y + closeItem->getContentSize().height / 2;
     closeItem->setPosition(Vec2(x, y));
   }
 
-  // create menu, it's an autorelease object
-  auto menu = Menu::create(closeItem, NULL);
+  const auto menu = Menu::create(closeItem, NULL);
   menu->setPosition(Vec2::ZERO);
   this->addChild(menu, 1);
 
-  /////////////////////////////
-  // 3. add your codes below...
-
-  // add a label shows "Hello World"
-  // create and initialize a label
-
   auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-  if (label == nullptr) {
+  if (nullptr == label) {
     problemLoading("'fonts/Marker Felt.ttf'");
   } else {
-    // position the label on the center of the screen
-    label->setPosition(
-      Vec2(origin.x + visibleSize.width / 2,
-           origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
+    label->setPosition(Vec2(visibleOrigin.x + visibleSize.width / 2,
+                            visibleOrigin.y + visibleSize.height -
+                              label->getContentSize().height));
     this->addChild(label, 1);
   }
 
-  // add "HelloWorld" splash screen"
-  auto sprite = Sprite::create("HelloWorld.png");
-  if (sprite == nullptr) {
+  const auto sprite = Sprite::create("HelloWorld.png");
+  if (nullptr == sprite) {
     problemLoading("'HelloWorld.png'");
   } else {
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x,
-                             visibleSize.height / 2 + origin.y));
-
-    // add the sprite as a child to this layer
+    sprite->setPosition(Vec2(visibleSize.width / 2 + visibleOrigin.x,
+                             visibleSize.height / 2 + visibleOrigin.y));
     this->addChild(sprite, 0);
   }
   return true;
@@ -116,12 +93,11 @@ HelloWorld::init()
 void
 HelloWorld::menuCloseCallback(Ref* pSender)
 {
-  // Close the cocos2d-x game scene and quit the application
   Director::getInstance()->end();
 
-  /*To navigate back to native iOS screen(if present) without quitting the
-   * application  ,do not use Director::getInstance()->end() as given
-   * above,instead trigger a custom event created in RootViewController.mm as
+  /*To navigate back to native iOS screen (if present) without quitting the
+   * application, do not use Director::getInstance()->end() as given
+   * above, instead trigger a custom event created in RootViewController.mm as
    * below*/
 
   // EventCustom customEndEvent("game_scene_close_event");
